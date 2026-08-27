8e2ae649a7fa6cf8535ee6a56da274629a909052
SilasP0 <possarle.silas16@gmail.com>
2026-08-27 14:25:29 -0300

    Refactor SQL table definitions for usuarios, reservas_mesas, pratos, and estoques

--- sql/script.sql
+++ sql/script.sql
@@ -1,27 +1,29 @@
 use db_luce;
 
 CREATE TABLE usuarios {
-id_user primary key,
-nome_completo Varchar(100),
-senha Number,
-cpf Number
+id_user  INT PRIMARY KEY,
+nome_completo VARCHAR(100),
+email VARCHAR(100),
+senha NUMBER,
+cpf NUMBER
 }
 
-CREATE TABLE reservas {
-id_reservas primary key,
-status Enum (disponivel,indisponivel),
+CREATE TABLE reservas_mesas {
+id_reservas INT PRIMARY KEY,
+status ENUM(disponivel,indisponivel),
 cadeiras Number
 }
 
 CREATE TABLE pratos {
-id_pratos primary key,
-num_prato Number,
+id_pratos INT PRIMARY KEY,
+nome_nome VARCHAR(100),
+num_prato NUMBER,
 status Enum (disponivel,indisponivel),
 foto_prato Varchar(1000)
 }
 
 CREATE TABLE estoques { 
-id_estoques primary key,
-num_ingrediente Number,
-status Enum(disponivel,indisponivel)
+id_estoques INT PRIMARY KEY,
+nome_produtos VARCHAR(100),
+num_ingrediente NUMBER
 }
